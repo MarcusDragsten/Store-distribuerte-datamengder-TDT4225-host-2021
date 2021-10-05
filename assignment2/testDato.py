e948003910b71a6dd3fe77419603e5da270978e7
Simen <simen_aabol@hotmail.com>
2021-10-05 14:28:00 +0200

    Testing

---
+++ assignment2/testDato.py
@@ -0,0 +1,22 @@
+import csv
+import os
+
+print("Hallo")
+
+# for (root,dirs,files) in os.walk('Test', topdown=true):
+#         print (root)
+#         print (dirs)
+#         print (files)
+#         print ('--------------------------------')
+
+# with open ('C:/Users/Simen/SkoleArbeid/Store,-distribuerte-datamengder/Store-distribuerte-datamengder-TDT4225-host-2021/assignment2/test.csv') as csv_file:
+#     csv_reader=csv.reader(csv_file, delimitre= ',')
+    
+
+# headerCount=8
+# lineCount=0
+# for row in csv_reader:
+#     if headerCount == lineCount:
+#       print(row[4])
+#     else:
+#         lineCount+=1
