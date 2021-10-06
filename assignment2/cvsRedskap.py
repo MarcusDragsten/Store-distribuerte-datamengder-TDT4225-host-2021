6daad973dcf41f824bba04fbd44011b6bf340270
Simen <simen_aabol@hotmail.com>
2021-10-06 12:29:43 +0200

    endret type

--- assignment2/cvsRedskap.py
+++ assignment2/cvsRedskap.py
@@ -1,3 +1,4 @@
+import csv
 class cvsRedskap:
 
 
@@ -5,19 +6,35 @@ class cvsRedskap:
     with open (path) as csv_file:
         firstLine =csv_file.readlines()[6]
         s= firstLine.split(",")
-        return s[5], s[6]
+        ut = s[5] + s[6]
+        return ut
         
   def datoSluttCSV(path):
     with open (path) as csv_file:
       finalLine =csv_file.readlines()[-1]
       s = finalLine.split(",")
-      return s[5], s[6]
+      ut= s[5]+ s[6]
+      return ut
   
 
   def godkjentLinerCSV(path):
        #Printer true hvis det er 2506 eller færre linjer
       return 2506 >= sum(1 for line in open(path))
 
+  def innholdPLT(path):
+        with open (path) as csv_file:
+          result = csv.reader(csv_file, delimiter=",")
+          aResult = []
+          jump=0
+          for row in result:
+                jump +=1
+                if jump == 7:
+                  print(row)
+                  aResult +=[row[0] + row[1] + row[3] + row[5] + row[6]]   
+          return aResult
+          
+  ut= innholdPLT("test.plt")
+  print(ut)
 
 
 
