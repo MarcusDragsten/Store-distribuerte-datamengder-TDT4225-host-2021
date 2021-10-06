8752544dce82da1cccb641f7b22d242efcb9c53a
Simen <simen_aabol@hotmail.com>
2021-10-06 14:45:20 +0200

    Fikset på dato-formatering

--- assignment2/testRedskap.py
+++ assignment2/testRedskap.py
@@ -16,15 +16,21 @@ godkjentLiner = redCvs.cvsRedskap.godkjentLinerCSV("test.plt")
 # print(godkjentLiner)
 
 
-startDatoTxt = redTxt.txtRedskap.datoStartTXT("labels.txt", 3)
+startDatoTxt = redTxt.txtRedskap.datoStartTXT("labels.txt", 0)
 # print("Halooo", startDatoTxt)
 
-sluttDatoTxt = redTxt.txtRedskap.datoSluttTXT("labels.txt", 1)
+sluttDatoTxt = redTxt.txtRedskap.datoSluttTXT("labels.txt", 0)
 # print(sluttDatoTxt)
 
 mode = redTxt.txtRedskap.hentMode("labels.txt", 1)
 # print(mode)
 
-print(datoStart)
+#print(datoStart)
 #print(startDatoTxt)
+
+print(datoSlutt)
+print(sluttDatoTxt)
+
+if datoStart == startDatoTxt and datoSlutt == sluttDatoTxt:
+    print("Seier")
 
