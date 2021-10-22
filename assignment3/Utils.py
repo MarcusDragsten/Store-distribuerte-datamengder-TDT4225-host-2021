743d13ad2d1fe899ef2249b99467c792c2d7c803
Simen <simen_aabol@hotmail.com>
2021-10-22 11:08:20 +0200

    Oppdatert kommentreing for å matche andre filer

--- assignment3/Utils.py
+++ assignment3/Utils.py
@@ -1,7 +1,9 @@
-#This class is for txt files
+# This file contains two utility-classes, txtUtils and pltUtils
+
+# This class is for txt files
 class txtUtils: 
     
-    #Get the start date from the index i
+    # Get the start date from the index i
     def dateStart(path, i):
         i+=1
         with open(path) as txtFil:
@@ -13,7 +15,7 @@ class txtUtils:
             newString = ''.join(map(str, L))
             return newString
 
-    #Get the end date from the index i
+    # Get the end date from the index i
     def dateEnd(path,i):
         i+=1
         with open(path) as txtFil:
@@ -25,21 +27,22 @@ class txtUtils:
             newString = ''.join(map(str, L))
             return newString
 
-    #Get the transportation mode from the index i
+    # Get the transportation mode from the index i
     def getMode(path,i):
         i+=1
         with open(path) as txtFil:
             mode = txtFil.readlines()[i]
             return(mode[40:len(mode)-1])
 
-    #Count the lines in a file
+    # Count the lines in a file
     def countLines(path):
             return  sum(1 for line in open(path)) -1
 
-#This class is for csv and in this case plt files
+
+# This class is for csv and in this case plt files
 class pltUtils:
         
-    #Get the start date 
+    # Get the start date 
     def dateStart(path):
         with open (path) as csv_file:
             firstLine =csv_file.readlines()[6]
@@ -47,7 +50,7 @@ class pltUtils:
             output = s[5] + " " + s[6][:8]
             return output
 
-    #Get the end date  
+    # Get the end date  
     def dateEnd(path):
         with open (path) as csv_file:
             finalLine =csv_file.readlines()[-1]
@@ -55,12 +58,12 @@ class pltUtils:
             output= s[5] + " " + s[6][:8]
             return output
 
-    #Returns true if there is a maximum of 2500 
+    # Returns true if there is a maximum of 2500 
     # lines of usable content in the file
     def approvedLines(path):
         return 2506 >= sum(1 for line in open(path))
 
-    #This method is not used. If the dataset is increased a lot, 
+    # This method is not used. If the dataset is increased a lot, 
     # it may be appropriate to use it
     def bufcount(filename):
         f = open(filename)                  
@@ -74,11 +77,11 @@ class pltUtils:
             buf = read_f(buf_size)
         return lines
 
-    #Returns the number of lines in the file with usable values
+    # Returns the number of lines in the file with usable values
     def countLinesWithValue(path):
             return sum(1 for line in open(path))-6
 
-    #Returns a tuple with the values from the line i
+    # Returns a tuple with the values from the line i
     def fileContent(path, i):
             i+=6
             with open (path) as csv_file:
